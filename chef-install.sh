11d199f6988bcd45e16fa6268316c8eec3f685a1
Conor Mullen <conor.mullen@gmail.com>
2012-02-18 11:13:29 -0500

    Formatting script

--- chef-install.sh
+++ chef-install.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-ssh -i $1 $2 "sudo yum -y install ruby && sudo yum -y install git && mkdir -p /home/ec2-user/chef/ && cd /home/ec2-user/chef && git clone git://github.com/conormullen/chef-bootstrap.git && sudo ruby /home/ec2-user/chef/chef-bootstrap/chef-solo-bootstrap.rb"
+ssh -i $1 $2 "sudo yum -y install ruby &&
+sudo yum -y install git && 
+mkdir -p /home/ec2-user/chef/ &&
+cd /home/ec2-user/chef && 
+git clone git://github.com/conormullen/chef-bootstrap.git && 
+sudo ruby /home/ec2-user/chef/chef-bootstrap/chef-solo-bootstrap.rb"
 
